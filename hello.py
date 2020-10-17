f47378a45aa34095d2a516934c7648be2510ce5b
KIT_07_206 <KIT_07_206@stud.kai.ru>
2020-10-17 17:34:19 +0300

    First app

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print('Hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/',methods=['GET'])
+
+def index():
+    return 'Hello World'
+
+
+DEBUG = True
+if __name__ == '__main__':
+    app.run()
